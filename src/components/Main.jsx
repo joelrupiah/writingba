7d001ece5f148160e0f90aef9588f46bcc15844a
joerupiah <rupiahjeremiah@gmail.com>
2023-01-09 09:37:40 -0500

    second commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Routes, Route } from "react-router-dom";
+import Header from '../inc/Header'
+import Footer from '../inc/Footer'
+
+import Home from './pages/home/Home'
+
+const Main = () => {
+	return (
+		<div className="App">
+
+			<Header />
+
+				<Routes>
+
+	            	<Route exact path='/' element={<Home />} />
+
+	            </Routes>
+
+			<Footer />
+
+		</div>
+	)
+}
+
+export default Main
