7e82e6a0c9360a06d15161bcc27f6edaa6251bee
joerupiah <rupiahjeremiah@gmail.com>
2023-01-14 13:56:28 -0500

    modal animation

--- src/components/integrations/modals/TestimonialsModal.jsx
+++ src/components/integrations/modals/TestimonialsModal.jsx
@@ -5,7 +5,7 @@ const TestimonialsModal = ({data, closeModal}) => {
 	return (
 		<>
 			<div className='modal-backdrop' onClick={closeModal}>
-				<div className="testimonials-modal">
+				<div className="testimonials-modal" data-aos="zoom-in">
 					<div className="modal-avatar-name">
 						<img className="modal-avatar" src={data.image} alt="modal avatar" />
 						<div className="modal-name">{data.name}</div>
